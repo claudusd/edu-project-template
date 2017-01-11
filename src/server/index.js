0124b9cb164b9236e615aee2df7bb06e823f6f86
ClementGilGit <clement_gil@orange.fr>
2017-01-11 17:24:18 +0100

    Creation_Routeur

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const api = require('./api.js');
 const config = require('./config.js');
+
+var app = express();
+app.use('/notes',api);
+app.listen(config.port, function () {
+  console.log('App listening on port 3000 ok!');
+});
+
