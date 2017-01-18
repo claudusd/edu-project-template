0a026eeefb418394448466acc087d1788713402b
Pierre Pic <pipic1@dpt_info.iut.local>
2017-01-18 17:33:42 +0100

    post and remove

--- src/server/api.js
+++ src/server/api.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const config = require('./config.js');
+const bodyParser = require('body-parser');
 const FindFiles = require("node-find-files");
+const fs = require("fs");
+const uuid = require("node-uuid");
 const api = express.Router();
 
+api.use(bodyParser.urlencoded({extended: false}))
+api.use(bodyParser.json())
 api.get('/',function(req, res) {
 	var files = [];
 
@@ -18,13 +23,40 @@ api.get('/',function(req, res) {
 		}
 	}).startSearch();
 });
-/*
+
 api.post('/',function(req, res) {
-    res.send('POST request\n');
+	uniqueId = uuid.v4();
+	fs.writeFile(config.data + "/" + uniqueId + ".json",JSON.stringify(req.body),err=>{
+		if(err)throw err;
+	})
+    res.send(201, JSON.stringify({id: uniqueId}));
 });
 
-api.put('/',function(req, res) {
-    res.send('PUT request\n');
+api.delete('/:id',function(req, res) {
+	var filename = config.data + "/" + req.params.id + ".json";
+	console.log("try to find: " + filename );
+	fs.exists(filename, (exists)=>{
+		if (exists) {
+			fs.unlink(filename);
+			res.status(200).send('File deleted\n');
+		}else{
+			res.status(404).send('File not found\n');
+		}
+	}) 
+    //res.send('PUT request\n');
 });
-*/
+
+/*
+api.get('/:id',function(req, res) {
+	var filename = config.data + "/" + req.params.id + ".json";
+	fs.exists(filename, (exists)=>{
+		if (exists) {
+			fs.unlink(filename);
+			res.status(204).send('File deleted\n');
+		}else{
+			res.status(404).send('File not found\n');
+		}
+	})
+});*/
+
  module.exports = api;
