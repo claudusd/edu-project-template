e0e5e4ba4c80cec77d51fdd3ae74f00925b4868a
ClementGilGit <clement_gil@orange.fr>
2017-02-07 17:21:18 +0100

    ccs2

--- src/app/components/OneNote.jsx
+++ src/app/components/OneNote.jsx
@@ -1,14 +1,15 @@
 import React, { Component, PropTypes } from 'react';
 import Client from './Client';
 import {browserHistory} from 'react-router';
+const Timestamp = require('react-timestamp');
 
 export default class OneNote extends Component {
     componentDidMount() {
         const c = new Client();
         c.find(this.setNote, this.props.params.id);
     }
-    ;
-            setNote(json) {
+
+    setNote(json) {
         this.setState({note: json});
     }
 
@@ -19,6 +20,7 @@ export default class OneNote extends Component {
         };
         this.setNote = this.setNote.bind(this);
         this.redirectAfter = this.redirectAfter.bind(this);
+
     }
 
     removeNote(id) {
@@ -33,22 +35,27 @@ export default class OneNote extends Component {
     render() {
         return(
                 <div className="col-md-6 col-md-offset-3">
-      
-                        <div className="hero-widget well well-sm text-center">
-                        
-                            <div className="text">
-                                <var>{this.state.note.title}</var>
+                
+                    <div className="hero-widget well well-sm text-center">
+                
+                        <div className="text">
+                            <var>{this.state.note.title}</var>
+                            <hr className="colorgraph"/>
+                            <div className="row">
                                 <label>{this.state.note.contenu}</label>
-                                <br/>
-                                <label>{this.state.note.date}</label>
-                                <br/>
-                                <button className="btn btn-danger" onClick={() => this.removeNote(this.state.note.id)}>Delete</button>
                             </div>
-                            <div className="options">
+                            <div className="row">
+                                <Timestamp time={this.state.date} format='date'/>
+                            </div>
+                            <div className="row">
+                                <a href="/"><button className="btn btn-primary" >Back</button></a>
+                                <button className="btn btn-danger" onClick={() => this.removeNote(this.state.note.id)}>Delete</button>                       
                             </div>
                         </div>
                     </div>
- 
+                
+                </div>
+
                 );
     }
 };
